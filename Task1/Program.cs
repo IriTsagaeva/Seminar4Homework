29d3ad3361bb15b712f638cbd2ef930fb1572588
Irina Tsagaeva <irulikk19@gmail.com>
2022-11-08 15:30:11 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿double RaiseToThePower (int number, int power)
+{
+    double result = 1;
+    int power_abs = Math.Abs(power);
+    for (int i = 0; i < power_abs; i++)
+    {
+        result = result * number;
+    }
+    if (power<0)
+    {
+        result = 1/result;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Пожалуйста, введите число");
+bool num_text = Int32.TryParse(Console.ReadLine(), out int number);
+Console.WriteLine("Пожалуйста, введите степень");
+bool pow_text = Int32.TryParse(Console.ReadLine(), out int power);
+if (num_text && pow_text)
+{
+    double result = RaiseToThePower(number,power);
+    Console.WriteLine($"{number} в степени {power} равно {result}");
+}
+else
+{
+    Console.WriteLine($"Пожалуйста, введите число. Вместо числа или степени введены некорректные данные");
+}
