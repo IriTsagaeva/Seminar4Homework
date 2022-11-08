8d0fa31afb60e6780d853179c2bb99cd4ff1b838
Irina Tsagaeva <irulikk19@gmail.com>
2022-11-08 17:06:56 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReturnDecimalsSum (int number)
+{
+    if (number<0)
+    {
+        number=number*(-1);
+    }
+    int sum = 0;
+    while (number/10!=0)
+    {
+        sum = sum + number%10;
+        number = number/10;        
+    }
+    sum = sum + number;
+    return sum;
+}
+
+Console.Clear();
+Console.WriteLine("Please, enter your number:");
+int sum = ReturnDecimalsSum (Int32.Parse(Console.ReadLine()));
+Console.WriteLine($"SUM = {sum}");
